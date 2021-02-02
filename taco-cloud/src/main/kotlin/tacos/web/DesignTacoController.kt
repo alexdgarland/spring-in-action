c232fd372d9bdaf483ebb62da0d76df249c4d0f9
Alex Garland <alex.d.garland@gmail.com>
2021-02-02 16:25:15 +0000

    Integrate validation with Order Form page

--- taco-cloud/src/main/kotlin/tacos/web/DesignTacoController.kt
+++ taco-cloud/src/main/kotlin/tacos/web/DesignTacoController.kt
@@ -78,5 +78,4 @@ class DesignTacoController {
         return "redirect:/orders/current"
     }
 
-
 }
--- taco-cloud/src/main/kotlin/tacos/web/OrderController.kt
+++ taco-cloud/src/main/kotlin/tacos/web/OrderController.kt
@@ -3,11 +3,13 @@ package tacos.web
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.validation.Errors
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import tacos.domain.OrderViewModel
-
+import javax.validation.Valid
 
 @Controller
 @RequestMapping("/orders")
@@ -23,7 +25,10 @@ class OrderController {
     }
 
     @PostMapping
-    fun processOrder(orderViewModel: OrderViewModel): String {
+    fun processOrder(@Valid @ModelAttribute("order") orderViewModel: OrderViewModel, errors: Errors): String {
+        if (errors.hasErrors()) {
+            return "orderForm"
+        }
         logger.info("Order submitted: ${orderViewModel.toOrder()}")
         return "redirect:/"
     }
