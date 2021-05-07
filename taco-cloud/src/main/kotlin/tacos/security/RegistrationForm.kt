9d63215f74487b46f46bf1b6a20b4d8504cee510
Alex Garland <alex.d.garland@gmail.com>
2021-05-07 21:36:12 +0100

    Add basic integration test for Registration form (and link from home page)

--- taco-cloud/src/main/kotlin/tacos/security/RegistrationForm.kt
+++ taco-cloud/src/main/kotlin/tacos/security/RegistrationForm.kt
@@ -2,17 +2,41 @@ package tacos.security
 
 import org.springframework.security.crypto.password.PasswordEncoder
 import tacos.domain.User
-
-// TODO - validate fields from form (e.g. not null, max length) and escalate errors to page
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 data class RegistrationForm(
+    @get:NotBlank(message="Username is required")
+    @get:Size(max=50, message="Username cannot be more than 50 characters")
     val username: String? = null,
+
+    @get:NotBlank(message="Password is required")
+    // No point in validating length here as what gets stored in the database is output of encoder
     val password: String? = null,
+
+    @get:NotBlank(message="Fullname is required")
+    @get:Size(max=100, message="Fullname cannot be more than 100 characters")
     val fullname: String? = null,
+
+    @get:NotBlank(message="Street is required")
+    @get:Size(max=100, message="Street cannot be more than 100 characters")
     val street: String? = null,
+
+    @get:NotBlank(message="City is required")
+    @get:Size(max=100, message="City cannot be more than 100 characters")
     val city: String? = null,
+
+    @get:NotBlank(message="State is required")
+    @get:Size(min=2, max=2, message="State must be exactly two characters")
     val state: String? = null,
+
+    @get:NotBlank(message="Zip code is required")
+    @get:Size(max=10, message="Zip code cannot be more than 10 characters")
     val zip: String? = null,
+
+    @get:NotBlank(message="Phone number is required")
+    @get:Size(max=20, message="Phone number cannot be more than 20 characters")
+    // TODO - Could potentially do more meaningful validation of phone number format here
     val phone: String? = null
 ) {
     fun toUser(passwordEncoder: PasswordEncoder) = User(
--- taco-cloud/src/test/kotlin/tacos/web/RegistrationControllerIT.kt
+++ taco-cloud/src/test/kotlin/tacos/web/RegistrationControllerIT.kt
@@ -1,11 +1,43 @@
 package tacos.web
 
-// TODO - write this test
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.testcontainers.junit.jupiter.Testcontainers
+import tacos.data.PostgresContainerTestInitializer
 
+private const val REGISTER_URL_TEMPLATE = "/register"
+
+@Testcontainers
+@SpringBootTest(properties = ["spring.main.allow-bean-definition-overriding=true"])
+@ContextConfiguration(initializers = [PostgresContainerTestInitializer::class])
+@AutoConfigureMockMvc
 class RegistrationControllerIT {
 
-    fun garbage() {
-        println("Hello world")
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun testHomePageIsAccessibleWithoutAuthorization() {
+        mockMvc
+            .perform(MockMvcRequestBuilders.get(REGISTER_URL_TEMPLATE))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.view().name("registrationForm"))
+            .andExpectHasString("Register")
+            .andExpectHasString("Username")
+            .andExpectHasString("Password")
+            .andExpectHasString("Confirm password")
+            .andExpectHasString("Full name")
+            .andExpectHasString("Street")
+            .andExpectHasString("City")
+            .andExpectHasString("State")
+            .andExpectHasString("Zip")
+            .andExpectHasString("Phone")
     }
 
 }
